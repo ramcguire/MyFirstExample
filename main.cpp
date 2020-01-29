5cedde1dcb0277e979cba998449df90b970573f8
Ryan McGuire <ryanadmcguire@gmail.com>
2020-01-29 11:35:25 -0800

    add number input

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 
 using std::cin;
 using std::cout;
+using std::endl;
 
 int main()
 {
+    int num1, num2;
     cout<<"Hi, please enter two numbers: ";
+    cin >> num1; 
+    cin >> num2;
+    cout << "Numbers were " << num1 << " and " << num2 << endl;
 
     return 0;
 }
