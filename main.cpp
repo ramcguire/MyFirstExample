d2cd606e5726184d72b49078f47896c0affc0add
Ryan McGuire <ryanadmcguire@gmail.com>
2020-02-05 11:31:13 -0800

    fix conflicts

--- main.cpp
+++ main.cpp
@@ -9,12 +9,7 @@ int main()
     int num1, num2;
     cout<<"Hi, please enter two numbers: ";
     cin >> num1 >> num2; 
-    cout << "Numbers were " << num1 << " and " << num2 << endl;
-
-    int x,y;
-
-    cin >> x >> y;
-    cout << x + y << endl;
+    cout << num1 + num2 << endl;
 
     return 0;
 }
